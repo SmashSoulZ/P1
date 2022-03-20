921fe580eeb23c973cc290c16ffb626a919a7187
Andre22703 <andre.ribeiro@ipvc.pt>
2022-03-20 18:12:57 +0000

    Primeiro Comit 1

--- src/main/java/com/mycompany/p1/P1.java
+++ src/main/java/com/mycompany/p1/P1.java
@@ -12,6 +12,6 @@ package com.mycompany.p1;
 public class P1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Worldddd!");
     }
 }
